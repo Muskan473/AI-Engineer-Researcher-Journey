65d883b2a56fb154df5435f84e0ad41c9eba6ef4
Muskan Sahani <anishasahani473@gmail.com>
2025-07-22 14:26:51 +0530

    Fixed summarization model string error

---
+++ NLP_LLM/text_summarizer_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("Text Summarizer App")
+st.write("Enter text below to summarize:")
+
+@st.cache_resource
+def load_summarizer():
+    return pipeline("summarization",model="sshleifer/distilbart-cnn-12-6")
+
+summarizer = load_summarizer()
+
+user_input = st.text_area("Enter text to summarize:", height=300)
+
+if st.button("Summarize"):
+    if len(user_input) > 0:
+        summary = summarizer(user_input, max_length=150, min_length=30, do_sample=False)
+        st.subheader("Summary:")
+        st.write(summary[0]['summary_text'])
+    else:
+        st.warning("Please enter some text to summarize.")
